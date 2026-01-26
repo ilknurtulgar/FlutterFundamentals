61b4374f70ec463d988f662ce413d62d0892b7a4
İlknur Tulgar <ilknur001-@hotmail.com>
2026-01-26 11:53:18 +0300

    gradient added for pageview

--- ui_widgets/page_controller/lib/page_controller_view.dart
+++ ui_widgets/page_controller/lib/page_controller_view.dart
@@ -9,32 +9,56 @@ class PageControllerView extends StatefulWidget {
 }
 
 class _PageControllerViewState extends State<PageControllerView> {
-  final _controller = PageController(initialPage: 0,viewportFraction: 1);
+  final _controller = PageController(initialPage: 0, viewportFraction: 1);
   int currentPage = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView.builder(scrollDirection: Axis.horizontal,
-      controller: _controller,
-      itemCount: kingdomPages.length,
-      itemBuilder: (context, index){
-        return Container(color: pageColors[currentPage % pageColors.length] ,child: Column(mainAxisAlignment: MainAxisAlignment.center,children: [
-          Image.asset(kingdomPages[index].imagePath,fit: BoxFit.cover,height: MediaQuery.of(context).size.height * 0.4, width: double.infinity,),
-          SizedBox(height: 30,),
-          Text(kingdomPages[index].title,style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
-          SizedBox(height: 15,),
-          Text(kingdomPages[index].subtitle, style: TextStyle(fontSize: 16, color: Colors.grey),),
-          
-
-        ],),);
-      },
-      pageSnapping: true,
-      onPageChanged: (index){
-        setState(() {
-        currentPage = index;
-
-        });
-      },
+      body: PageView.builder(
+        scrollDirection: Axis.horizontal,
+        controller: _controller,
+        itemCount: kingdomPages.length,
+        itemBuilder: (context, index) {
+          return Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  pageColors[currentPage % pageColors.length],
+                  Colors.white,
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  kingdomPages[index].imagePath,
+                  fit: BoxFit.cover,
+                  height: MediaQuery.of(context).size.height * 0.4,
+                  width: double.infinity,
+                ),
+                SizedBox(height: 30),
+                Text(
+                  kingdomPages[index].title,
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 15),
+                Text(
+                  kingdomPages[index].subtitle,
+                  style: TextStyle(fontSize: 16, color: Colors.grey),
+                ),
+              ],
+            ),
+          );
+        },
+        pageSnapping: true,
+        onPageChanged: (index) {
+          setState(() {
+            currentPage = index;
+          });
+        },
       ),
     );
   }
